d2cc9aeda161fe5e50c7c7abfc8619fa9091da82
Akhil Raja <p.akhilraja@gmail.com>
2023-12-21 15:22:40 +0530

    Get Request - llm

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 import os
+from fastapi import FastAPI
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from fastapi.responses import StreamingResponse
+
+app = FastAPI()
 
 # Set the OpenAI API key
 openai_api_key = "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"
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-from langchain.document_loaders import PyPDFLoader
-
 # Load your text data using the TextLoader
 loader = PyPDFLoader("test-notebooks/sample.pdf")
 documents = loader.load()
-
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain.text_splitter import CharacterTextSplitter
 
 # Generate VectorDB using Chroma and OpenAIEmbeddings
 text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
 embeddings = OpenAIEmbeddings()
 docsearch = Chroma.from_documents(texts, embeddings)
-
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQA
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 # Create a ChatOpenAI instance for interactive chat using the OpenAI model
 llm = ChatOpenAI(
@@ -39,5 +40,14 @@ qa = RetrievalQA.from_chain_type(
     retriever=docsearch.as_retriever(),
 )
 
-# Run the QA system by providing a question to the chain
-qa.run("What is this document ?")
+@app.get("/qa")
+async def run_qa():
+    # Run the QA system by providing a question to the chain
+    question = "What is this document ?"
+    answer = qa.run(question)
+    return StreamingResponse(iter([answer]), media_type="text/plain")
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
