2f7d067a92337ba57e62f14763ed44ce0a2f11cd
Akhil Raja <p.akhilraja@gmail.com>
2023-12-21 15:40:07 +0530

    file input - added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
-from fastapi import FastAPI
+from typing import List
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import StreamingResponse
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
@@ -7,7 +9,6 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -15,37 +16,59 @@ app = FastAPI()
 openai_api_key = "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"
 os.environ["OPENAI_API_KEY"] = openai_api_key
 
-# Load your text data using the TextLoader
-loader = PyPDFLoader("test-notebooks/sample.pdf")
-documents = loader.load()
+# Function to process PDF and questions
+def process_qa(pdf_path: str, questions: List[str]):
+    # Load your text data using the TextLoader
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
 
-# Generate VectorDB using Chroma and OpenAIEmbeddings
-text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
-embeddings = OpenAIEmbeddings()
-docsearch = Chroma.from_documents(texts, embeddings)
+    # Generate VectorDB using Chroma and OpenAIEmbeddings
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    embeddings = OpenAIEmbeddings()
+    docsearch = Chroma.from_documents(texts, embeddings)
 
-# Create a ChatOpenAI instance for interactive chat using the OpenAI model
-llm = ChatOpenAI(
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
-    temperature=0,
-    openai_api_key=openai_api_key,
-)
+    # Create a ChatOpenAI instance for interactive chat using the OpenAI model
+    llm = ChatOpenAI(
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        temperature=0,
+        openai_api_key=openai_api_key,
+    )
 
-# Create a RetrievalQA chain using the ChatOpenAI model and the document retriever
-qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=docsearch.as_retriever(),
-)
+    # Create a RetrievalQA chain using the ChatOpenAI model and the document retriever
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=docsearch.as_retriever(),
+    )
 
-@app.get("/qa")
-async def run_qa():
-    # Run the QA system by providing a question to the chain
-    question = "What is this document ?"
-    answer = qa.run(question)
-    return StreamingResponse(iter([answer]), media_type="text/plain")
+    # Process each question and get answers
+    results = []
+    for question in questions:
+        answer = qa.run(question)
+        results.append({"question": question, "answer": answer})
+
+    return results
+
+@app.post("/qa")
+async def run_qa(pdf_file: UploadFile = File(...), questions: List[str] = None):
+    if questions is None:
+        questions = []
+    
+    # Save the uploaded PDF file to a temporary location
+    with open(pdf_file.filename, "wb") as pdf:
+        pdf.write(pdf_file.file.read())
+
+    try:
+        # Process PDF and questions
+        results = process_qa(pdf_file.filename, questions)
+        return results
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        # Clean up: Remove the temporary PDF file
+        os.remove(pdf_file.filename)
 
 if __name__ == "__main__":
     import uvicorn
